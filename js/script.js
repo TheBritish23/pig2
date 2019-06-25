556bd3c240f32d5c612b57227e4716ac49be55f5
Soro Cat <sorousha326@gmail.com>
2019-06-25 13:33:47 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 var translate = function(sentence){
   var vowels = ["a","e","i","o","u"]
   var splitstring = sentence.split("")
-  var pig = "way"
+  var pig = ["way"]
   for(x=0; x < splitstring; x++ ){
     vowels.forEach(function(letter){
       console.log(letter)
     if(splitstring[x]===letter)
-    if((x=="9")&&(x++)){
+
+    var newWord = splitstring.splice(x, splitstring.length)+(splitstring.slice(0,x)+"way");
+  });
+    console.log(letter)
+
+    // if((x=="q")&&(x++)){
+    //
+    // }
   };
-})
-  var newWord = splitstring.splice(x, splitstring.length)+(splitstring.slice(0,x)+"way"){
-    splitstring.push("way");
-    x = splitstring.length
-  };
-  console.log(letter)
+ output = splitstring.join("")+"way"
+ })
+ console.log(splitstring)
+ return splitstring;
 });
 
 
@@ -27,4 +32,10 @@ $(document).ready(function() {
 
     var splitstring = translate(words)
 
-  }
+    $('#output').append(splitstring)
+
+    $('#display').append(x);
+})
+
+
+  });
