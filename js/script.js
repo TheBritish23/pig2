e1e85076ae1adfec66155ba520c16e39f3389b48
Soro Cat <sorousha326@gmail.com>
2019-06-27 10:54:19 -0700

    add changes'

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 var translate = function(sentence){
   var vowels = ["a","e","i","o","u"]
   var splitstring = sentence.split("")
-  var pig = ["way"]
   var newWord
-  for(x=0; x < splitstring.length; x++ ){
+  for(x=0; x < splitstring.length; x++){
     vowels.forEach(function(letter){
       console.log(letter)
-    if(splitstring[x]===letter){
-     newWord = splitstring.splice(x, splitstring.length) + splitstring.splice(0,x).join("") + "way"
-  }
+      if (splitstring[0]===letter){
+        newWord = splitstring.join("") + "way";
+      }
+    else if(splitstring[x]===letter){
+        newWord = splitstring.splice(x, splitstring.length).join("") + splitstring.splice(0,x).join("") + "ay"
+      }
+    })
+   };
 
-  });
+     if((x=="q")&&(x++)){
 
-    // if((x=="q")&&(x++)){
-    //
-    // }
-  };
- output = splitstring.join("")+"way"
+     }
+ output = splitstring.join("") + "way"
  console.log(splitstring)
  return newWord
 };
@@ -27,15 +28,13 @@ var translate = function(sentence){
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-    var pig = parseInt($("input#year").val())
 
     var words = $('#words').val()
 
     var splitstring = translate(words)
 
     $('#output').append(splitstring)
+  })
 
-})
 
-
-  });
+    });
