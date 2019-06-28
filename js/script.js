9a47bfcd941a908322682a81af28ed767037b1b0
Soro Cat <sorousha326@gmail.com>
2019-06-28 10:52:21 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -3,24 +3,23 @@ var translate = function(sentence){
   var splitstring = sentence.split("")
   var newWord
   for(x=0; x < splitstring.length; x++){
+    console.log(splitstring.length)
     vowels.forEach(function(letter){
       console.log(letter)
       if (splitstring[0]===letter){
         newWord = splitstring.join("") + "way";
       }
-    else if(splitstring[x]===letter){
+      else if(splitstring[x]===letter){
         newWord = splitstring.splice(x, splitstring.length).join("") + splitstring.splice(0,x).join("") + "ay"
       }
-    })
-   };
-
-     if((x=="q")&&(x++)){
-
-     }
- output = splitstring.join("") + "way"
+      if((x=="q")&&(x+1==="u")){
+        newWord = splitstring.splice(x+1, splitstring.length).join("") + splitstring.splice(0,x+1).join("") + "ay"
+      }
+    });
  console.log(splitstring)
  return newWord
 };
+}
 
 
 
@@ -36,5 +35,4 @@ $(document).ready(function() {
     $('#output').append(splitstring)
   })
 
-
-    });
+});
